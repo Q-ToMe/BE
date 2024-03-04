49656e9e96e88ac4c5d187d407423d7d6be79b11
Yeji <mnbvcxzyj@gmail.com>
2024-03-04 23:17:34 +0900

    initial setting

---
+++ src/main/java/com/example/qtome_be/QToMeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.qtome_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QToMeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QToMeBeApplication.class, args);
+    }
+
+}
